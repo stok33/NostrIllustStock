208aca754b2d937021e79de5905e61dc8dc6221a
すとくさん/stoksann <131473459+stok33@users.noreply.github.com>
2023-06-27 22:47:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -34,21 +34,70 @@ const searchPosts = async () => {
   await relay.connect(relayUrl);
 
   /* Q-3: Relayオブジェクトのメソッドを使って、イベントを購読してみよう */
-  const sub = relay.sub([
-  		{
-			"kinds": [1],
-			"limit": 100,
-			"#t":["illust"],
-			"authors": [npub]
-			// 作者の公開鍵
-	
-		}
-  ]);
+  //kind0を購読
+  const sub0 = relay.sub([
+    {
+        "kinds":[0],
+        "authors":[npub]
+    }
 
+  ]);
+  //kind1にフィルター（100イベント、illustタグつき、指定した公開鍵からの投稿）つけて購読
+  const sub = relay.sub([
+    {
+        "kinds": [1],
+        "limit": 100,
+        "#t":["illust"],
+        "authors": [npub]
+        // 作者の公開鍵
+	
+    }
+  ]);
+  
+  const profContainer = document.getElementById("profContainer");
+  profContainer.innerHTML = ""; // コンテナをクリア
+    
   const illustContainer = document.getElementById("illustContainer");
   illustContainer.innerHTML = ""; // コンテナをクリア
 
   // メッセージタイプごとにリスナーを設定できる
+  
+  //kind0の方
+  sub0.on("event", (pf) => {
+    console.log(pf);
+
+    try {
+      const profcontent = JSON.parse(pf.content); // JSON文字列をオブジェクトに変換
+      const profname = profcontent.name; // content内のnameの値を取得
+      const profabout = profcontent.about; // content内のaboutの値を取得
+      console.log(profname);
+      
+      // プロフィールを表示するための要素を作成
+      const kind0Container = document.createElement("div");
+      
+      // プロフィールを表示するための部分にnameとaboutの要素を追加
+      const nameElement = document.createElement("div");
+      nameElement.textContent = profname;
+      kind0Container.appendChild(nameElement);
+
+      const aboutElement = document.createElement("div");
+      aboutElement.textContent = profabout;
+      kind0Container.appendChild(aboutElement);
+
+      // プロフィールコンテナに追加
+      profContainer.appendChild(kind0Container);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+
+
+  sub0.on("eose", () => {
+    console.log("****** EOSE ******");
+  });
+
+  //kind1の方
 　sub.on("event", (ev) => {
     console.log(ev);
 
